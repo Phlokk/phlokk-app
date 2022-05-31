794fb5c5f081e0ecd03a9f0b67e495c819998290
r00t-us3r <r00tus3r.90@gmail.com>
2022-05-31 23:58:52 +0100

    upload media file

--- app/src/services/saveMedia.js
+++ app/src/services/saveMedia.js
@@ -1,5 +1,6 @@
 import FormData from "form-data";
 import axios, { axiosVideo } from "../redux/apis/axiosDeclaration";
+import * as SecureStore from "expo-secure-store";
 
 export const saveMediaToStorage = (source, description) => {
   new Promise(async (resolve, reject) => {
@@ -7,24 +8,35 @@ export const saveMediaToStorage = (source, description) => {
     console.log("save source", source);
 
     let formData = new FormData();
-    formData.append("file", source, {
-      name: source,
+
+    let split = source.split('/');
+    let fileName = split[(split.length - 1)];
+
+    formData.append("video", {
+      name: fileName,
       type: "video/mp4",
-      uri: source.replace("file://", ""),
-    });
-    formData.append("description", description);
+      uri: source,
+    }, fileName);
     console.log("Sending.....");
     console.log(formData);
     console.log("------------------");
 
-    let url = "/api/test/post";
-    axiosVideo
-      .post(url, formData)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((error) => {
-        console.log(error.response);
-      });
+    const user = await SecureStore.getItemAsync("user");
+    if (user) {
+      const parsedUser = JSON.parse(user);
+
+      let url = "/api/test/post";
+      fetch("https://phlokk.com"+url,
+          {
+            method: 'POST',
+            body: formData,
+            headers: {
+              Authorization: 'Bearer '+parsedUser.token
+            }
+          }
+      )
+    } else {
+      console.log('no bearer')
+    }
   });
 };
