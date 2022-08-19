07f645f9ba8f086230afe2d95d559485ce8a620e
Nathan Nelson <ceo@phlokk.com>
2022-08-19 09:00:12 -0500

    fixed unknown token

--- app/src/screens/activity/index.js
+++ app/src/screens/activity/index.js
@@ -3,10 +3,8 @@ import ActivityNavBar from "../../components/general/activityNav";
 import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import { FontAwesome5 } from '@expo/vector-icons'; 
-
 import colors from "../../../config/colors";
-import CustomAlert from "../../components/Alerts/customAlert";
-import SavePostScreen from "../savePost";
+
 
 export default function ActivityScreen({ navigation }) {
   const dispatch = useDispatch();
