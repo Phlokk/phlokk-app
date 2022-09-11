b302556f7e8b43e944bed5d5af11d65befc6285f
Nathan Nelson <ceo@phlokk.com>
2022-09-11 06:24:59 -0500

    blank commit

--- app/src/screens/search/liveurlScreen/livestreamUrl.js
+++ app/src/screens/search/liveurlScreen/livestreamUrl.js
@@ -1,20 +1,34 @@
-import {useRef} from "react";
-import {Button, PermissionsAndroid, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import colors from '../../../../config/colors'
-
-
-
-
-
-
+import axios from "axios";
+import { Video } from "expo-av";
+import { useRef, useState } from "react";
+import {
+  Button,
+  PermissionsAndroid,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import colors from "../../../../config/colors";
 
 export default function LiveStreamURLScreen() {
-
+  // const { streamList, setStreamList } = useState({});
+  // axios.get("http://88.97.6.186:8088/index.php").then(async (response) => {
+  // setStreamList(response.data);
+  // });
 
   return (
     <View style={styles.container}>
-      
-
+      <Video
+        source={{
+          uri: "http://88.97.6.186:8088/hls/demo_620.m3u8",
+        }}
+        rate={1.0}
+        volume={1.0}
+        resizeMode="cover"
+        shouldPlay
+        style={{ width: "100%", height: "100%" }}
+      />
     </View>
   );
 }
@@ -23,7 +37,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.primary,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
